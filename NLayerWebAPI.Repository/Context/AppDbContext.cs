8f20ae7240e961bb80197038674939b400b5a9cd
Canberk Coşkun <121981226+canberkcoskun1@users.noreply.github.com>
2023-07-04 16:21:43 +0300

    ProductFeature configuration, Product Configuration definitions have been made. ApplyConfiguration done in AppDbContext.

--- NLayerWebAPI.Repository/Context/AppDbContext.cs
+++ NLayerWebAPI.Repository/Context/AppDbContext.cs
@@ -3,6 +3,7 @@ using NLayerWebAPI.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,5 +18,12 @@ namespace NLayerWebAPI.Repository.Context
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
-    }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
 }
