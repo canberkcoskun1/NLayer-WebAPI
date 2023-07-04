479153d254a451cc8c67ba8df676f64abf39bc23
Canberk Coşkun <121981226+canberkcoskun1@users.noreply.github.com>
2023-07-04 16:58:05 +0300

    UnitOfWork saveChanges method defined.

---
+++ NLayerWebAPI.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using NLayerWebAPI.Core.UnitOfWorks;
+using NLayerWebAPI.Repository.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerWebAPI.Repository.UnitOfWorks
+{
+	public class UnitOfWork : IUnifOfWork
+	{
+		private readonly AppDbContext _context;
+
+		public UnitOfWork(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public void Commit()
+		{
+			_context.SaveChanges();
+		}
+
+		public async Task CommitAsync()
+		{
+			await _context.SaveChangesAsync();
+		}
+	}
+}
